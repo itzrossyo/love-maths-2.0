e7f7ebfe57dbe53c31b71a125a1dfd45e47657b8
itzrossyo <Rossespley@gmail.com>
2023-06-20 22:39:38 +0100

    setup structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function runGame() {}
+
+function checkAnswer() {}
+
+function calclateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWronganswer() {}
+
+function displayAdditonQustion() {}
+function displaySubtractQustion() {}
+
+function displayMultiplyQustion() {}
+function displayDivisonQustion() {}
