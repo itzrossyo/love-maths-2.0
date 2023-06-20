294f1ba7eaf51e0268a7c936ea9102b47ce72af7
itzrossyo <Rossespley@gmail.com>
2023-06-20 22:48:09 +0100

    added event lisnter to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+//wait for the dom to finsh loading beofre running the game
+//get the button elements and add event listners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByTagName("button");
+
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("you clicked submit!");
+      } else {
+        let gameType = this.getAttribute("data-Type");
+        alert(`you clicked ${gameType}`);
+      }
+    });
+  }
+});
+
 function runGame() {}
 
 function checkAnswer() {}
@@ -9,7 +27,9 @@ function incrementScore() {}
 function incrementWronganswer() {}
 
 function displayAdditonQustion() {}
+
 function displaySubtractQustion() {}
 
 function displayMultiplyQustion() {}
+
 function displayDivisonQustion() {}
